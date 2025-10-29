8b5711ca3ef788fc370ed0eedae4630fc2e080e9
sohamk005 <soham2525kulkarni@gmail.com>
2025-10-29 18:44:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.post('/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRouter)
 app.use('/api/course', express.json(), courseRouter)
 app.use('/api/user', express.json(), userRouter)
-app.post('/stripe', express.raw({type: 'appplication/json'}), stripeWebhooks)
+app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
 const PORT = process.env.PORT || 5000
 
